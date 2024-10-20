a20e34d4b37ead6953ee55c9abc67947194773ab
pony <gafulungatie@gmail.com>
2024-10-20 23:50:24 +0800

    Update build.yml

---
+++ build.ps1
@@ -0,0 +1,65 @@
+# Set the location to the script's directory
+Push-Location -Path $PSScriptRoot
+
+# Project name and safe name
+$workspace = "Community.PowerToys.Run.Plugin.JetBrainsRecents"
+$projectName = "Community.PowerToys.Run.Plugin.JetBrainsRecents"
+$safeProjectName = "JetBrains_Recent_Plugin"
+
+# Temporary directory for builds
+$tempDir = "./$workspace/out/temp"
+$releaseBasePath = "./$workspace/bin"
+
+# Build configurations and platforms
+$configurations = @("Release")
+$platforms = @("x64", "ARM64")
+
+# Assembly name
+$assembly = "Community.PowerToys.Run.Plugin.$safeProjectName"
+
+# Ensure the temporary directory exists
+if (-not (Test-Path $tempDir)) {
+    New-Item -ItemType Directory -Path $tempDir | Out-Null
+}
+
+
+foreach ($configuration in $configurations) {
+    foreach ($platform in $platforms) {
+        Write-Host "Building $projectName for $platform in $configuration configuration"
+
+        # Build the project
+        dotnet build -c $configuration /p:Platform=$platform
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host "Build failed for $platform in $configuration configuration." -ForegroundColor Red
+            exit $LASTEXITCODE
+        }
+
+        # Define release path
+        $releasePath = "$releaseBasePath/$platform/$configuration/net8.0-windows"
+
+        # Clear temporary directory
+        Remove-Item "$tempDir/*" -Recurse -Force -ErrorAction Ignore
+
+        # Define items to copy
+        $items = @(
+            "$releasePath/$assembly.deps.json",
+            "$releasePath/$assembly.dll",
+            "$releasePath/plugin.json",
+            "$releasePath/Images"
+        )
+
+        # Copy items to temporary directory
+        Write-Host "Copy $items"
+        Copy-Item $items "$tempDir" -Recurse -Force -ErrorAction Stop
+        # Define output zip file name
+        $outputZip = "./$($projectName)/out/$($projectName)-$($configuration)-$platform.zip"
+
+        # Compress the directory
+        Compress-Archive -Path "$tempDir/*" -DestinationPath $outputZip -Force
+    }
+}
+
+Write-Host "Build completed successfully." -ForegroundColor Green
+
+# Return to the original directory
+Pop-Location
